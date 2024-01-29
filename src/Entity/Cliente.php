b6821800b55f0282adcd72b462780626513863f2
Arkaitz Calvo Saldias <acalvosald@educacion.navarra.es>
2024-01-29 11:18:19 +0100

    Agregado cliente para insertar api keys

---
+++ src/Entity/Cliente.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClienteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClienteRepository::class)]
+class Cliente
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $apiKey = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getApiKey(): ?string
+    {
+        return $this->apiKey;
+    }
+
+    public function setApiKey(string $apiKey): static
+    {
+        $this->apiKey = $apiKey;
+
+        return $this;
+    }
+}
